e10f9699fb529fc45b42444e4e8f93988774a882
zameer <ahamedsmr@yahoo.com>
2013-04-14 21:54:44 +0530

    Update Book.cs

--- MVCFour/Models/Book.cs
+++ MVCFour/Models/Book.cs
@@ -5,6 +5,16 @@ using System.Web;
 
 namespace MVCFour.Models
 {
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Friend> Friends { get; set; }
+    }
+    public class Friend
+    {
+        public string Name { get; set; }
+    }
     public class Book
     {
         public int ID { get; set; }
